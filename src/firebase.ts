23f7ee00b95c5530ac8c006122f4b18227a955d0
yukajii <chr.jungblut@gmail.com>
2025-07-07 12:45:49 +0300

    Firebase config added

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app";
 
 export const app = initializeApp({
-  apiKey:            "YOUR-API-KEY",
-  authDomain:        "your.firebaseapp.com",
-  projectId:         "your",
-  storageBucket:     "your.appspot.com",
-  messagingSenderId: "123",
-  appId:             "1:123:web:abc",
-  measurementId:     "G-XXXX"
+  apiKey: "AIzaSyCEw80e897zNASO4Zw6rqyudCjzo4jzaAI",
+  authDomain: "leagues-2790f.firebaseapp.com",
+  projectId: "leagues-2790f",
+  storageBucket: "leagues-2790f.firebasestorage.app",
+  messagingSenderId: "998292692484",
+  appId: "1:998292692484:web:29d733fc7e032d4f739f51",
+  measurementId: "G-HQ0EDCZCQ9"
 });
